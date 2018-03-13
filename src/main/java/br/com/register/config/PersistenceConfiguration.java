4b617dbc00a1ce408d3d94e75c42708ba73146f8
Antonio Sampaio <antonio.sampaio.n@gmail.com>
2018-03-12 21:03:44 -0300

    Adiciona Spring Boot Actuator

--- src/main/java/br/com/register/config/PersistenceConfiguration.java
+++ src/main/java/br/com/register/config/PersistenceConfiguration.java
@@ -1,13 +1,12 @@
 package br.com.register.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * Created by asampaio on 07/01/18.
@@ -19,13 +18,6 @@ public class PersistenceConfiguration {
     @ConfigurationProperties(prefix = "spring.datasource")
     @Primary
     public DataSource dataSource(){
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "datasource.flyway")
-    @FlywayDataSource
-    public DataSource flywayDataSource(){
         return DataSourceBuilder.create().build();
     }
 
